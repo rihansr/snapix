b74ac7e92dde041528bbcf240ae525f426a43861
rihansr <rx23026@gmail.com>
2025-01-19 23:55:08 +0600

    implement method channel to fetch gallery albums and photots from native Android and iOS

--- android/app/src/main/kotlin/com/example/snapix/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/snapix/MainActivity.kt
@@ -1,5 +1,26 @@
 package com.example.snapix
 
 import io.flutter.embedding.android.FlutterActivity
+import com.example.snapix.helpers.GalleryHelper
+import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.plugin.common.MethodChannel
 
-class MainActivity: FlutterActivity()
+
+class MainActivity: FlutterActivity(){
+    private val channel = "gallery_images"
+
+    override fun configureFlutterEngine(flutterEngine: FlutterEngine) {
+        super.configureFlutterEngine(flutterEngine)
+
+        // Gallery
+        MethodChannel(flutterEngine.dartExecutor.binaryMessenger, channel).setMethodCallHandler { call, result ->
+            if (call.method == "getFoldersAndImages") {
+                val galleryHelper = GalleryHelper(this)
+                val foldersAndImages = galleryHelper.getFoldersAndImages()
+                result.success(foldersAndImages)
+            } else {
+                result.notImplemented()
+            }
+        }
+    }
+}
